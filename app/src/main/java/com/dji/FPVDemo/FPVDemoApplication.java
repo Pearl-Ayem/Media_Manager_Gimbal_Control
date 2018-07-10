24f16c1159d96be2ce18b6fe88330008b0513a2c
Pearl <pearlayem@gmail.com>
2018-07-10 14:17:50 -0700

    FPVDemoApplication implemented

---
+++ app/src/main/java/com/dji/FPVDemo/FPVDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dji.FPVDemo;
+
+/**
+ * Created by Pearl on 2018-07-10.
+ */
+
+import android.app.Application;
+public class FPVDemoApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
